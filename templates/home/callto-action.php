e30966e52f415401cc60ee6ef36ed458486aed23
Simple User <u456988@gen138.hs.shared.masterhost.ru>
2016-04-12 22:51:17 +0300

    updated banner

--- templates/home/callto-action.php
+++ templates/home/callto-action.php
@@ -1,17 +1,26 @@
-<?php 
-	global $pinnacle; 
-?>
+<?php
+                $post_type = 'banner';
+                //Number of posts to display
+                $post_count = 1;
+                $args = array( 'post_type' => $post_type,
+                                'posts_per_page' => $post_count);
+                $loop = new WP_Query( $args );
+                while ( $loop->have_posts() ) : $loop->the_post();
+                ?>
 <div class="home_calltoaction home-margin clearfix home-padding">
 	<div class="kt-home-call-to-action panel-row-style-wide-feature">
 		<div class="call-container clearfix">
 			<div class="kt-cta">
 				<div class="col-md-10 kad-call-title-case">
-					<h1 class="kad-call-title"><?php if(isset($pinnacle['home_action_text'])) echo esc_html($pinnacle['home_action_text']);?></h1>
+					<h1 class="kad-call-title"><?php the_title();?></h1>
 				</div>
 				<div class="col-md-2 kad-call-button-case">
-					<a href="<?php if(isset($pinnacle['home_action_link'])) echo esc_url($pinnacle['home_action_link']);?>" class="kad-btn-primary kad-btn lg-kad-btn"><?php if(isset($pinnacle['home_action_text_btn'])) echo esc_html($pinnacle['home_action_text_btn']);?></a>   	
+					<a href="<?php echo get_field('link');?>" class="kad-btn-primary kad-btn lg-kad-btn"><?php echo get_field('text');?></a>   	
 				</div>
 			</div>
 		</div><!--container-->
 	</div><!--call class-->
 </div>
+<?php
+                endwhile;
+            ?>
